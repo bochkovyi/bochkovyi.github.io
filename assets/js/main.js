f665c50a1da0f7a6b827b07ce760ea7e29840369
bochkovyi <bochkovyi@gmail.com>
2018-03-21 09:19:24 +0200

    Added CMS with Router

--- assets/js/main.js
+++ assets/js/main.js
@@ -278,7 +278,8 @@ Vue.component('projects-code', {
                    {value: "imaginator", name: "Imaginator", description: "There is (was) a possibility to access pictures of the shutdown Panoramio by direct link. App downloads up to 20 pictures at random per call.", type: "Node.js"},
                    {value: "web_services", name: "Web Services", description: "Very simple Node api that uses mysql library", type: "Node.js"},
                    {value: "bochkovyi.github.io/tree/master/projects/live-stream", name: "Live Stream Car Washery", description: "App that uses YouTube API and shows some Vue.js components", type: "Vue.js"},
-                   {value: "Battleship", name: "Battleship", description: "Good and famous Battleship game with random ship placement", type: "React with TypeScript"}
+                   {value: "Battleship", name: "Battleship", description: "Good and famous Battleship game with random ship placement", type: "React with TypeScript"},
+                   {value: "CMS-with-router", name: "CMS with Router", description: "JSON sitemap fetched at app start and defines the content", type: "React with Router"}
                   ]};
   }
 });
@@ -298,7 +299,8 @@ Vue.component('projects-live', {
       {value: "projects/mapapp", name: "LocationTracker", type: "Angular"}, 
       {value: "projects/orchid", name: "Orchid", type: "Vue.js"},
       {value: "projects/live-stream", name: "Live Stream Car Washery", type: "Vue.js"},
-      {value: "projects/battleship", name: "Battleship", type: "React with TypeScript"}
+      {value: "projects/battleship", name: "Battleship", type: "React with TypeScript"},
+      {value: "projects/cms-with-router", name: "CMS with Router", type: "React with Router"}
     ]};
   }
 });
---
+++ projects/cms-with-router/static/js/4.d430141a.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([4],{34:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var r=t(0),a=t.n(r),o=t(33),u=t.n(o),c=t(9);it("renders without crashing",function(){var e=document.createElement("div");u.a.render(a.a.createElement(c.default,null),e),u.a.unmountComponentAtNode(e)})}});
+//# sourceMappingURL=4.d430141a.chunk.js.map
